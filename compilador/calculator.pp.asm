cb98442a96a527a531937d1d1fbd393662942611
stalker21 <leonardosalzano@gmail.com>
2017-11-23 01:12:16 -0300

    cambios al 23-11

---
+++ compilador/calculator.pp.asm
@@ -0,0 +1,58 @@
+	.file	"calculator.c"
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"I know how to add! 31 + 11 is %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movq	%rsi, -32(%rbp)
+	movl	$11, %esi
+	movl	$31, %edi
+	call	add_numbers
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.globl	add_numbers
+	.type	add_numbers, @function
+add_numbers:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	add_numbers, .-add_numbers
+	.ident	"GCC: (Debian 6.3.0-18) 6.3.0 20170516"
+	.section	.note.GNU-stack,"",@progbits
