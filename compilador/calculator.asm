eea5bce547f7e085eae7e112ec10b602a9706dce
stalker21 <leonardosalzano@gmail.com>
2017-11-27 16:15:54 -0300

    cambios al 27-11

---
+++ compilador/calculator.asm
@@ -0,0 +1,59 @@
+	.file	"calculator.c"
+	.intel_syntax noprefix
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"I know how to add! 31 + 11 is %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	sub	rsp, 32
+	mov	DWORD PTR -20[rbp], edi
+	mov	QWORD PTR -32[rbp], rsi
+	mov	esi, 11
+	mov	edi, 31
+	call	add_numbers
+	mov	DWORD PTR -4[rbp], eax
+	mov	eax, DWORD PTR -4[rbp]
+	mov	esi, eax
+	lea	rdi, .LC0[rip]
+	mov	eax, 0
+	call	printf@PLT
+	mov	eax, 0
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.globl	add_numbers
+	.type	add_numbers, @function
+add_numbers:
+.LFB1:
+	.cfi_startproc
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	mov	DWORD PTR -4[rbp], edi
+	mov	DWORD PTR -8[rbp], esi
+	mov	edx, DWORD PTR -4[rbp]
+	mov	eax, DWORD PTR -8[rbp]
+	add	eax, edx
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	add_numbers, .-add_numbers
+	.ident	"GCC: (Debian 6.3.0-18) 6.3.0 20170516"
+	.section	.note.GNU-stack,"",@progbits
